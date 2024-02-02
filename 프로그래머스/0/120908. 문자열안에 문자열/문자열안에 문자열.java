7aa1107ad560389dd9a7b1ee58bdf1e51d4fc0bb
Seong Min Cho <122413012+EnjoyTime18@users.noreply.github.com>
2024-02-02 18:36:34 +0900

    [level 0] Title: 문자열안에 문자열, Time: 0.03 ms, Memory: 72.6 MB -BaekjoonHub

--- 프로그래머스/0/120908. 문자열안에 문자열/문자열안에 문자열.java
+++ 프로그래머스/0/120908. 문자열안에 문자열/문자열안에 문자열.java
@@ -1,7 +1,7 @@
 class Solution {
     public int solution(String str1, String str2) {
         //문제 의도 자체가 라이브러리 메소드 찾아서 사용하라는게 눈에 보이는 문제 유형
-        if(str1.contains(str2)) {
+        if(str1.contains(str2)) { // String contains() 컨틴스 메소드 사용
         	return 1;
         } else {
         	return 2;
