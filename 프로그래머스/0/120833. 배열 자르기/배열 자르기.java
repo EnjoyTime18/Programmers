f3e853585cd203c92da3e9bdc545f9aa593e90ec
Seong Min Cho <122413012+EnjoyTime18@users.noreply.github.com>
2024-02-02 12:37:34 +0900

    [level 0] Title: 배열 자르기, Time: 0.02 ms, Memory: 73.6 MB -BaekjoonHub

--- 프로그래머스/0/120833. 배열 자르기/배열 자르기.java
+++ 프로그래머스/0/120833. 배열 자르기/배열 자르기.java
@@ -1,34 +1,35 @@
 class Solution {
     public int[] solution(int[] numbers, int num1, int num2) {
         
-        int index = 0;
+//         int index = 0;
 		
-		for(int i=0; i<numbers.length; i++) {
-			if(i >= num1 && i <= num2) {
-				index++;
-			}
-		}
+// 		for(int i=0; i<numbers.length; i++) {
+// 			if(i >= num1 && i <= num2) {
+// 				index++;
+// 			}
+// 		}
 				
-		int[] number = new int[index];
-		int numindex = 0;
+// 		int[] number = new int[index];
+// 		int numindex = 0;
 		
-		for(int i=0; i<numbers.length; i++) {
-			if(i >= num1 && i <= num2) {
-				//number[i] = numbers[i]; //number[i] 여기서 에러 터짐 주의. 조건이 성립하면 인덱스 0으로 고정값 주어서 값을 배열에 넣어야 하는데 i값으로 주면 인덱스가 0이 아닌 i값으로 인덱스가 잡혀 에러 발생
-				number[numindex] = numbers[i];
-				numindex++;
-			}
-		}
+// 		for(int i=0; i<numbers.length; i++) {
+// 			if(i >= num1 && i <= num2) {
+// 				//number[i] = numbers[i]; //number[i] 여기서 에러 터짐 주의. 조건이 성립하면 인덱스 0으로 고정값 주어서 값을 배열에 넣어야 하는데 i값으로 주면 인덱스가 0이 아닌 i값으로 인덱스가 잡혀 에러 발생
+// 				number[numindex] = numbers[i];
+// 				numindex++;
+// 			}
+// 		}
        
-       return number;
+//        return number;
         
-//         int[] answer = new int[num2-num1+1];
-//         int idx = 0;
-//         for(int i = num1; i<=num2; i++){
-//             answer[idx] = numbers[i];
-//             idx++;
-//         }
+        int[] answer = new int[num2-num1+1];
+        int idx = 0;
         
-//         return answer;
+        for(int i = num1; i<=num2; i++) {
+            answer[idx] = numbers[i];
+            idx++;
+        }
+        
+        return answer;
     }
 }
