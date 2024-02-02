9f47132bb6fb42b303d6834dbadc032946eb5096
Seong Min Cho <122413012+EnjoyTime18@users.noreply.github.com>
2024-02-02 18:34:44 +0900

    [level 0] Title: 문자열안에 문자열, Time: 0.03 ms, Memory: 73.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120908. 문자열안에 문자열/문자열안에 문자열.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int solution(String str1, String str2) {
+        //문제 의도 자체가 라이브러리 메소드 찾아서 사용하라는게 눈에 보이는 문제 유형
+        if(str1.contains(str2)) {
+        	return 1;
+        } else {
+        	return 2;
+        }
+    }
+}
