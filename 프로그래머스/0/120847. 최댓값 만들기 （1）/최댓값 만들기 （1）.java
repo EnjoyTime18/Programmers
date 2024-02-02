466fe69b3d53ca64dab0e2a7dbabfc6aad303e2c
Seong Min Cho <122413012+EnjoyTime18@users.noreply.github.com>
2024-02-02 11:20:58 +0900

    [level 0] Title: 최댓값 만들기 (1), Time: 0.34 ms, Memory: 78 MB -BaekjoonHub

---
+++ 프로그래머스/0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.java
@@ -0,0 +1,36 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+class Solution {
+	public int solution(int[] numbers) {
+		
+		int max1 = 0;
+		int max2 = 0;
+		int maxindex = 0;
+		
+		Arrays.sort(numbers); //먼저 주어진 numbers 배열에서 최대 값 찾기 쉽게 오름차순으로 배열을 정렬해주는 작업
+		
+		List<Integer> intList = new ArrayList<>();        
+		for (int element : numbers) { // numbers 배열 요소들 순차적으로 ArrayList 배열에 요소 추가
+			intList.add(element);        
+		}
+				
+		for(int i=0; i<intList.size(); i++) { // 첫번째 최대값 배열 요소 탐색
+			if(intList.get(i) > max1) {
+				max1 = intList.get(i);
+				maxindex = i;
+			}
+		}
+		
+		intList.remove(maxindex); // 첫번째 최대값 알아냈으니 두번째 최대값 찾기 위해 첫번째 최대값 배열에서 제거
+		
+		for(int i=0; i<intList.size(); i++) { // 두번째 최대값 배열 요소 탐색
+			if(intList.get(i) > max2) {
+				max2 = intList.get(i);
+			}
+		}
+		
+		return max1 * max2;
+	}
+}
