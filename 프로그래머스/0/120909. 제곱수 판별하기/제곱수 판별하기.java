34739f0a12692b1408cf025ed5e42fb3427c32a0
Seong Min Cho <122413012+EnjoyTime18@users.noreply.github.com>
2024-02-08 20:47:11 +0900

    [level 0] Title: 제곱수 판별하기, Time: 0.03 ms, Memory: 75 MB -BaekjoonHub

--- 프로그래머스/0/120909. 제곱수 판별하기/제곱수 판별하기.java
+++ 프로그래머스/0/120909. 제곱수 판별하기/제곱수 판별하기.java
@@ -1,12 +1,23 @@
 class Solution {
     public int solution(int n) {
-        
-        for(int i=1; i<=n; i++) {
-        	if(i * i == n) {
-        		return 1;
-        	}
-        }
-        
-        return 2;               
-    }
+		//Math.sqrt() -> 인자의 숫자를 넣으면 그 숫자가 어떤 숫자의 제곱인지 제곱근을 구해주는 수학 메소드. 즉, 특정값의 제곱근을 구함.
+		//Math.sqrt(25) -> 25의 제곱근은 5이다.
+		
+		//정답1
+		int m = (int) Math.sqrt(n); // 12, 31
+		return n % m == 0 ? 1 : 2;
+
+		//정답2
+//		int a = (int) Math.sqrt(n);		
+//		return (a * a) == n ? 1 : 2;
+
+		// 아래 for문은 정답은 맞으나 비효율적 for문
+//        for(int i=1; i<=n; i++) {
+//        	if((i * i) == n) {
+//        		return 1;
+//        	}
+//        }
+//        
+//        return 2;               
+	}
 }
