26ebde4c44e9bfa487d25f34fce9aa57d707e24e
Seong Min Cho <122413012+EnjoyTime18@users.noreply.github.com>
2024-02-08 20:50:21 +0900

    [level 0] Title: 제곱수 판별하기, Time: 0.03 ms, Memory: 78.8 MB -BaekjoonHub

--- 프로그래머스/0/120909. 제곱수 판별하기/제곱수 판별하기.java
+++ 프로그래머스/0/120909. 제곱수 판별하기/제곱수 판별하기.java
@@ -1,7 +1,7 @@
 class Solution {
     public int solution(int n) {
 		//Math.sqrt() -> 인자의 숫자를 넣으면 그 숫자가 어떤 숫자의 제곱인지 제곱근을 구해주는 수학 메소드. 즉, 특정값의 제곱근을 구함.
-		//Math.sqrt(25) -> 25의 제곱근은 5이다. 해당 메소드는 반환타입이 double형 이므로 정수형으로 변환하려면 명시적 형변환 필수
+		//Math.sqrt(25) -> 25의 제곱근은 5이다. 해당 메소드는 반환 타입이 double형 이므로 정수형으로 변환하려면 명시적 형변환 필수
 		
 		//정답1
 		int m = (int) Math.sqrt(n); // 12, 31
