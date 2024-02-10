3e25a3073be75efe7a8dedcf1410d9cef99da0d5
Seong Min Cho <122413012+EnjoyTime18@users.noreply.github.com>
2024-02-10 21:44:36 +0900

    [level 0] Title: 영어가 싫어요, Time: 0.11 ms, Memory: 76.6 MB -BaekjoonHub

--- 프로그래머스/0/120894. 영어가 싫어요/영어가 싫어요.java
+++ 프로그래머스/0/120894. 영어가 싫어요/영어가 싫어요.java
@@ -1,7 +1,7 @@
 class Solution {
     public long solution(String numbers) {
 
-		String number = numbers.replace("zero", "0").replace("one", "1").replace("two", "2").replace("three", "3")
+		numbers = numbers.replace("zero", "0").replace("one", "1").replace("two", "2").replace("three", "3")
 				.replace("four", "4").replace("five", "5").replace("six", "6").replace("seven", "7")
 				.replace("eight", "8").replace("nine", "9");
 
@@ -9,6 +9,6 @@ class Solution {
         //아래는 int 형으로 형변환 후 또 바로 long 타입으로 명시적 형변환하여 2번 형변환 과정을 거쳤기 때문에 정답이 아닌듯.
 		//return (long) Integer.parseInt(number);
         
-		return Long.parseLong(number);
+		return Long.parseLong(numbers);
 	}
 }
