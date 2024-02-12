77e97be2a2836541d46688264889af6b7106fba7
Seong Min Cho <122413012+EnjoyTime18@users.noreply.github.com>
2024-02-12 14:07:37 +0900

    [level 0] Title: 문자열 출력하기, Time: 128.79 ms, Memory: 63.2 MB -BaekjoonHub

--- 프로그래머스/0/181952. 문자열 출력하기/문자열 출력하기.java
+++ 프로그래머스/0/181952. 문자열 출력하기/문자열 출력하기.java
@@ -4,5 +4,6 @@ public class Solution {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);      
         System.out.println(sc.next());
+        sc.close();
     }
 }
