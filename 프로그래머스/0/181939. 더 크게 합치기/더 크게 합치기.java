c69fb06951c6bd88d1abaf3451dbdc920251c3a4
Seong Min Cho <122413012+EnjoyTime18@users.noreply.github.com>
2024-02-12 20:41:00 +0900

    [level 0] Title: 더 크게 합치기, Time: 1.18 ms, Memory: 73.8 MB -BaekjoonHub

--- 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.java
+++ 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.java
@@ -1,12 +1,7 @@
 class Solution {
-    public int solution(int a, int b) {
-		String s1 = ""; 
-		String s2 = ""; 
-		
-		s1 += String.valueOf(a);
-		s1 += String.valueOf(b);
-		s2 += String.valueOf(b);
-		s2 += String.valueOf(a);
+    public int solution(int a, int b) {		
+		String s1 = String.valueOf(a) + String.valueOf(b); // 문자 + 문자 = 문자를 연결해준다.
+		String s2 = String.valueOf(b) + String.valueOf(a);
 
 		if (Integer.parseInt(s1) > Integer.parseInt(s2)) {
 			return Integer.parseInt(s1);
