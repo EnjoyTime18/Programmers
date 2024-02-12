cf7beb9d09826aeced3052fc2e9fc14d77887405
Seong Min Cho <122413012+EnjoyTime18@users.noreply.github.com>
2024-02-12 13:44:09 +0900

    [level 0] Title: n보다 커질 때까지 더하기, Time: 0.02 ms, Memory: 73.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/181884. n보다 커질 때까지 더하기/n보다 커질 때까지 더하기.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int solution(int[] numbers, int n) {
+        int total = 0;
+        
+        for(int num : numbers) {
+        	total += num;
+        	if(total > n) {
+        		break;
+        	}
+        }
+        
+        return total;
+        
+    }
+}
