373a964ac25a74fa6b8c8f386319fca8394a6f35
Seong Min Cho <122413012+EnjoyTime18@users.noreply.github.com>
2024-02-12 13:44:59 +0900

    [level 0] Title: n보다 커질 때까지 더하기, Time: 0.01 ms, Memory: 71.1 MB -BaekjoonHub

--- 프로그래머스/0/181884. n보다 커질 때까지 더하기/n보다 커질 때까지 더하기.java
+++ 프로그래머스/0/181884. n보다 커질 때까지 더하기/n보다 커질 때까지 더하기.java
@@ -1,15 +1,14 @@
 class Solution {
     public int solution(int[] numbers, int n) {
-        int total = 0;
-        
-        for(int num : numbers) {
-        	total += num;
-        	if(total > n) {
-        		break;
-        	}
-        }
-        
-        return total;
-        
-    }
+		int total = 0;
+
+		for (int num : numbers) {
+			total += num;
+			if (total > n) {
+				break;
+			}
+		}
+
+		return total;
+	}
 }
