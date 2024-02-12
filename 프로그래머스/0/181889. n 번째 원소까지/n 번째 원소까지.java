c1c0478b56fc2bddd853577818f84316a868b0c1
Seong Min Cho <122413012+EnjoyTime18@users.noreply.github.com>
2024-02-12 19:31:42 +0900

    [level 0] Title: n 번째 원소까지, Time: 0.01 ms, Memory: 74.9 MB -BaekjoonHub

--- 프로그래머스/0/181889. n 번째 원소까지/n 번째 원소까지.java
+++ 프로그래머스/0/181889. n 번째 원소까지/n 번째 원소까지.java
@@ -1,17 +1,21 @@
+import java.util.Arrays;
+
 class Solution {
     public int[] solution(int[] num_list, int n) {
-		int index = 0;
-		for (int i = 0; i < n; i++) {
-			index++;
-		}
-
-		int[] num = new int[index];
-		int indx = 0;
-		for (int i = 0; i < n; i++) {
-			num[indx] = num_list[i];
-			indx++;
-		}
-
-		return num;
+//		int index = 0;
+//		for (int i = 0; i < n; i++) {
+//			index++;
+//		}
+//
+//		int[] num = new int[index];
+//		int indx = 0;
+//		for (int i = 0; i < n; i++) {
+//			num[indx] = num_list[i];
+//			indx++;
+//		}
+//
+//		return num;
+        
+		return Arrays.copyOfRange(num_list, 0, n);
 	}
 }
