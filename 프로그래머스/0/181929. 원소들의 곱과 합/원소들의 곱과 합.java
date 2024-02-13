7cdc60432243b548d5b2069913ca5ba4435d8a30
Seong Min Cho <122413012+EnjoyTime18@users.noreply.github.com>
2024-02-13 12:46:36 +0900

    [level 0] Title: 원소들의 곱과 합, Time: 0.05 ms, Memory: 71.7 MB -BaekjoonHub

--- 프로그래머스/0/181929. 원소들의 곱과 합/원소들의 곱과 합.java
+++ 프로그래머스/0/181929. 원소들의 곱과 합/원소들의 곱과 합.java
@@ -1,22 +1,26 @@
 class Solution {
     public int solution(int[] num_list) {
-		int mult = 0;
-		for (int i = 0; i < 1; i++) {
-			mult = num_list[i];
-			for (int j = 1; j < num_list.length; j++) {
-				mult *= num_list[j];
-			}
-		}
+//		int mult = 0;
+//		for (int i = 0; i < 1; i++) {
+//			mult = num_list[i];
+//			for (int j = 1; j < num_list.length; j++) {
+//				mult *= num_list[j];
+//			}
+//		}
+//
+//		int total = 0;
+//		for (int n : num_list) {
+//			total += n;
+//		}
 
 		int total = 0;
-		for (int n : num_list) {
-			total += n;
+		int mult = 1;
+
+		for (int i : num_list) {
+			total += i;
+			mult *= i;
 		}
 
-		if (mult < Math.pow(total, 2)) {
-			return 1;
-		} else {
-			return 0;
-		}
+		return (mult < Math.pow(total, 2)) ? 1 : 0;
 	}
 }
