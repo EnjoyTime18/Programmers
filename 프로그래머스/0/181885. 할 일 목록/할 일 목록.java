a0e86b9ef569f8d0d6d7d9d98f0e34c60b97780a
Seong Min Cho <122413012+EnjoyTime18@users.noreply.github.com>
2024-02-14 12:29:27 +0900

    [level 0] Title: 할 일 목록, Time: 0.03 ms, Memory: 78.4 MB -BaekjoonHub

--- 프로그래머스/0/181885. 할 일 목록/할 일 목록.java
+++ 프로그래머스/0/181885. 할 일 목록/할 일 목록.java
@@ -4,8 +4,8 @@ class Solution {
     public String[] solution(String[] todo_list, boolean[] finished) {
 		ArrayList<String> list = new ArrayList<String>();
 
-		for(int i=0; i<todo_list.length; i++) {
-			if(finished[i] == false) {
+		for (int i = 0; i < todo_list.length; i++) {
+			if (finished[i] == false) {
 				list.add(todo_list[i]);
 			}
 		}
