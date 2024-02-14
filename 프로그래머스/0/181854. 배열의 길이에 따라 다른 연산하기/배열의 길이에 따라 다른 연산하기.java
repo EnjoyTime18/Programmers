ac4f55ecea3183d14f125f7179f77657c9d254cb
Seong Min Cho <122413012+EnjoyTime18@users.noreply.github.com>
2024-02-14 14:04:07 +0900

    [level 0] Title: 배열의 길이에 따라 다른 연산하기, Time: 0.03 ms, Memory: 72 MB -BaekjoonHub

--- 프로그래머스/0/181854. 배열의 길이에 따라 다른 연산하기/배열의 길이에 따라 다른 연산하기.java
+++ 프로그래머스/0/181854. 배열의 길이에 따라 다른 연산하기/배열의 길이에 따라 다른 연산하기.java
@@ -1,30 +1,42 @@
 class Solution {
     public int[] solution(int[] arr, int n) {
-		int[] answer = new int[arr.length];
-		int index = 0;
-		
+//		int[] answer = new int[arr.length];
+//		int index = 0;
+//		
+//		if (arr.length % 2 == 1) {
+//			for (int i = 0; i < arr.length; i++) {
+//				if(i % 2 == 0) {
+//					answer[index] = arr[i] + n;
+//					index++;
+//				} else {
+//					answer[index] = arr[i];
+//					index++;
+//				}
+//			}
+//		} else {
+//			for (int i = 0; i < arr.length; i++) {
+//				if(i % 2 == 1) {
+//					answer[index] = arr[i] + n;
+//					index++;
+//				} else {
+//					answer[index] = arr[i];
+//					index++;
+//				}
+//			}
+//		}
+//
+//		return answer;
+
 		if (arr.length % 2 == 1) {
-			for (int i = 0; i < arr.length; i++) {
-				if(i % 2 == 0) {
-					answer[index] = arr[i] + n;
-					index++;
-				} else {
-					answer[index] = arr[i];
-					index++;
-				}
+			for (int i = 0; i < arr.length; i += 2) {
+				arr[i] += n;
 			}
 		} else {
-			for (int i = 0; i < arr.length; i++) {
-				if(i % 2 == 1) {
-					answer[index] = arr[i] + n;
-					index++;
-				} else {
-					answer[index] = arr[i];
-					index++;
-				}
+			for (int i = 1; i < arr.length; i += 2) {
+				arr[i] += n;
 			}
 		}
 
-		return answer;
+		return arr;
 	}
 }
