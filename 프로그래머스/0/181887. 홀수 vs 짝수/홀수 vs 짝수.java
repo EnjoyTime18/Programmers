b984cf3f431f2d6bd3805d00d4f037f327d7b3d1
Seong Min Cho <122413012+EnjoyTime18@users.noreply.github.com>
2024-02-14 12:54:27 +0900

    [level 0] Title: 홀수 vs 짝수, Time: 0.03 ms, Memory: 73 MB -BaekjoonHub

---
+++ 프로그래머스/0/181887. 홀수 vs 짝수/홀수 vs 짝수.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int solution(int[] num_list) {
+		int odd = 0;
+		int ever = 0;
+
+		for (int i = 0; i < num_list.length; i++) {
+			int n = num_list[i];
+			if ((i + 1) % 2 == 1) {
+				odd += n;
+			} else {
+				ever += n;
+			}
+		}
+
+		if (odd > ever) {
+			return odd;
+		} else if (odd < ever) {
+			return ever;
+		} else {
+			return odd;
+		}
+		
+	}
+}
