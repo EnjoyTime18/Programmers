1d0ff2b99c80d049e11bb3d9463620d3431179c2
Seong Min Cho <122413012+EnjoyTime18@users.noreply.github.com>
2024-02-16 14:39:19 +0900

    [level 0] Title: x 사이의 개수, Time: 34.53 ms, Memory: 95.1 MB -BaekjoonHub

--- 프로그래머스/0/181867. x 사이의 개수/x 사이의 개수.java
+++ 프로그래머스/0/181867. x 사이의 개수/x 사이의 개수.java
@@ -7,6 +7,7 @@ class Solution {
 		// split() 2번째 인자값은 리턴받을 배열의 길이를 제한함.
 		// 음수일 경우 맨 뒤의 마지막 구분자(빈값)에 대해 split을 모두 진행 or 모든 구분값을 나눠서 배열로 반환한다.
 		// limit를 음수를 주면 모든 요소를 다 리스트에 포함시켜서 반환함.
+        // 더 이상 나누어 질수 없는 구분자에 대해서도 "" 빈 문자열로 배열 요소에 포함해 split을 진행
 		for (String s : myString.split("x", -1)) {
 			list.add(s.length());
 		}
