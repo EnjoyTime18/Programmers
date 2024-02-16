ea7e697018321c12348b1de9de127983f6645799
Seong Min Cho <122413012+EnjoyTime18@users.noreply.github.com>
2024-02-16 20:57:46 +0900

    [level 0] Title: 5명씩, Time: 0.03 ms, Memory: 74.8 MB -BaekjoonHub

--- 프로그래머스/0/181886. 5명씩/5명씩.java
+++ 프로그래머스/0/181886. 5명씩/5명씩.java
@@ -2,12 +2,12 @@ import java.util.ArrayList;
 
 class Solution {
     public String[] solution(String[] names) {
-        ArrayList<String> arylist = new ArrayList<String>();
-        
-        for(int i=0; i<names.length; i=i+5) {
-        	arylist.add(names[i]);
-        }
-        
-        return arylist.toArray(new String[arylist.size()]);
-    }
+		ArrayList<String> list = new ArrayList<String>();
+
+		for (int i = 0; i < names.length; i = i + 5) {
+			list.add(names[i]);
+		}
+
+		return list.toArray(new String[list.size()]);
+	}
 }
