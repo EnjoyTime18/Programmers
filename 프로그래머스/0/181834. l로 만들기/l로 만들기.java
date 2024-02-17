ef12995680d885fb59fcf4e2c26866071e87eebc
Seong Min Cho <122413012+EnjoyTime18@users.noreply.github.com>
2024-02-17 20:54:11 +0900

    [level 0] Title: l로 만들기, Time: 12.14 ms, Memory: 84.4 MB -BaekjoonHub

--- 프로그래머스/0/181834. l로 만들기/l로 만들기.java
+++ 프로그래머스/0/181834. l로 만들기/l로 만들기.java
@@ -1,15 +1,17 @@
 class Solution {
     public String solution(String myString) {
-		String answer = "";
+// 		String answer = "";
 
-		for (int i = 0; i < myString.length(); i++) {
-			if (myString.charAt(i) < 'l') { // 아스키 코드 소문자 기준 : 'a' = 97, 'ㅣ' = 108
-				answer += 'l';
-			} else {
-				answer += myString.charAt(i);
-			}
-		}
+// 		for (int i = 0; i < myString.length(); i++) {
+// 			if (myString.charAt(i) < 'l') { // 아스키 코드 소문자 기준 : 'a' = 97, 'ㅣ' = 108
+// 				answer += 'l';
+// 			} else {
+// 				answer += myString.charAt(i);
+// 			}
+// 		}
 
-		return answer;
+// 		return answer;
+        
+        return myString.replaceAll("[a-k]", "l");
 	}
 }
