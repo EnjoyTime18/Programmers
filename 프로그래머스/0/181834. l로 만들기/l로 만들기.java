ea49af5df787f7214dfd69999282b026b8d0d11c
Seong Min Cho <122413012+EnjoyTime18@users.noreply.github.com>
2024-02-17 20:52:14 +0900

    [level 0] Title: l로 만들기, Time: 673.05 ms, Memory: 381 MB -BaekjoonHub

---
+++ 프로그래머스/0/181834. l로 만들기/l로 만들기.java
@@ -0,0 +1,15 @@
+class Solution {
+    public String solution(String myString) {
+		String answer = "";
+
+		for (int i = 0; i < myString.length(); i++) {
+			if (myString.charAt(i) < 'l') { // 아스키 코드 소문자 기준 : 'a' = 97, 'ㅣ' = 108
+				answer += 'l';
+			} else {
+				answer += myString.charAt(i);
+			}
+		}
+
+		return answer;
+	}
+}
