c0359e6b2d0522bad4dbfb9bf6ba1432a2ce842b
Seong Min Cho <122413012+EnjoyTime18@users.noreply.github.com>
2024-02-18 13:26:18 +0900

    [level 0] Title: 문자열 잘라서 정렬하기, Time: 18.83 ms, Memory: 76.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/181866. 문자열 잘라서 정렬하기/문자열 잘라서 정렬하기.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
+class Solution {
+    public String[] solution(String myString) {
+//		String[] s = myString.split("x");
+//		Arrays.sort(s);
+//
+//		ArrayList<String> list = new ArrayList<String>();
+//
+//		for (String str : s) {
+//			list.add(str);
+//		}
+//		
+//		return list.toArray(new String[list.size()]);
+
+		ArrayList<String> list = new ArrayList<String>();
+
+		for (String str : myString.split("x")) {
+			if (!str.equals("")) { // str = "" -> str.equals("") 이 자체로만 보면 true이지만 ! 부정연산자 때문에 결과적으로 true에서 반대결과를 갖는 false가 됨.
+				list.add(str);    // 그래서 빈 문자열인 경우 배열 요소에 넣지 않음.
+			}			
+		}
+
+		Collections.sort(list);
+		
+		return list.toArray(new String[list.size()]);
+	}
+}
