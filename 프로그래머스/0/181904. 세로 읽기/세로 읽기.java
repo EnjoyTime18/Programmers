3d0aacea5d6e6f05c4488492f75d4002e8b3c301
Seong Min Cho <122413012+EnjoyTime18@users.noreply.github.com>
2024-02-18 20:36:48 +0900

    [level 0] Title: 세로 읽기, Time: 13.29 ms, Memory: 82.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181904. 세로 읽기/세로 읽기.java
@@ -0,0 +1,13 @@
+class Solution {
+    public String solution(String my_string, int m, int c) {
+		String answer = "";
+		int midex = m;  
+
+		for (int i = 0; i < my_string.length(); i += m) { // (i += m)에서 m 값이 바뀌지 않고 4로 계속 유지하는게 포인트
+			answer += my_string.substring(i, midex).charAt(c - 1);
+			midex += m; // 전체 문자열에서 substring()로 인해 앞에서 부터 4글자씩 계속 잘라야 하기 떄문에 끊어야할 끝 index는 4씩 계속 누적 증가해야 함.
+		}
+
+		return answer;
+	}
+}
