cb461f89b428863ec9b45e0c7ee0a4757ebe1f8b
Seong Min Cho <122413012+EnjoyTime18@users.noreply.github.com>
2024-02-20 14:10:10 +0900

    [level 0] Title: 글자 지우기, Time: 0.41 ms, Memory: 78.1 MB -BaekjoonHub

--- 프로그래머스/0/181900. 글자 지우기/글자 지우기.java
+++ 프로그래머스/0/181900. 글자 지우기/글자 지우기.java
@@ -1,11 +1,39 @@
-class Solution {
-    public String solution(String my_string, int[] indices) {
-		char[] c = my_string.toCharArray();
+import java.util.Arrays;
 
-		for (int index : indices) {
-			c[index] = ' ';
+class Solution {
+    	public String solution(String my_string, int[] indices) {
+		// 방법1
+//		char[] c = my_string.toCharArray();
+//
+//		for (int index : indices) {
+//			c[index] = ' ';
+//		}
+//
+//		return String.valueOf(c).replace(" ", "");
+
+		// 방법2
+//		String answer = "";
+//		String[] tmp = my_string.split("");
+//
+//		for (int index : indices) {
+//			tmp[index] = "";
+//		}
+//
+//		for (String x : tmp) {
+//			answer += x;
+//		}
+//		
+//		return answer;
+
+		// 방법3
+		StringBuilder sb = new StringBuilder(my_string);
+		Arrays.sort(indices);
+
+		for (int i = 0; i < indices.length; ++i) {
+			int index = indices[i];
+			sb.deleteCharAt(index - i); // 삭제 할 때마다 문자열의 길이가 자동으로 줄어들기 때문에 - i 필수
 		}
 
-		return String.valueOf(c).replace(" ", "");
+		return sb.toString();
 	}
 }
