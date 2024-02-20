f6710be71728b043493be89ba1364ababe8dbc1e
Seong Min Cho <122413012+EnjoyTime18@users.noreply.github.com>
2024-02-20 14:47:46 +0900

    [level 0] Title: 1로 만들기, Time: 0.04 ms, Memory: 71.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181880. 1로 만들기/1로 만들기.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int solution(int[] num_list) {
+		int answer = 0;
+
+		for (int n : num_list) {
+			while (true) {
+				if (n == 1) {
+					break;
+				}
+
+				if (n % 2 == 0) {
+					n = n / 2;
+					answer++;
+				} else {
+					n = (n - 1) / 2;
+					answer++;
+				}
+			}
+		}
+
+		return answer;
+	}
+}
