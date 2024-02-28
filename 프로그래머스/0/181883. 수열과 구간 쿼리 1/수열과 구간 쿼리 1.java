41951012a6bdcd115cc92cad52567dead4af3728
Seong Min Cho <122413012+EnjoyTime18@users.noreply.github.com>
2024-02-28 12:15:43 +0900

    [level 0] Title: 수열과 구간 쿼리 1, Time: 2.54 ms, Memory: 85.1 MB -BaekjoonHub

--- 프로그래머스/0/181883. 수열과 구간 쿼리 1/수열과 구간 쿼리 1.java
+++ 프로그래머스/0/181883. 수열과 구간 쿼리 1/수열과 구간 쿼리 1.java
@@ -2,7 +2,7 @@ class Solution {
     public int[] solution(int[] arr, int[][] queries) {
 		for (int row = 0; row < queries.length; row++) {
 			for (int i = queries[row][0]; i <= queries[row][1]; i++) {
-				arr[i] = arr[i] + 1;
+				arr[i]++;
 			}
 		}
 
