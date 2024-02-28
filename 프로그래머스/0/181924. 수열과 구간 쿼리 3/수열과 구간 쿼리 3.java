164e11d0ced62f21ab9031adddb51b0d6af6bd39
Seong Min Cho <122413012+EnjoyTime18@users.noreply.github.com>
2024-02-28 12:28:09 +0900

    [level 0] Title: 수열과 구간 쿼리 3, Time: 0.07 ms, Memory: 74.3 MB -BaekjoonHub

--- 프로그래머스/0/181924. 수열과 구간 쿼리 3/수열과 구간 쿼리 3.java
+++ 프로그래머스/0/181924. 수열과 구간 쿼리 3/수열과 구간 쿼리 3.java
@@ -1,13 +1,19 @@
 class Solution {
     public int[] solution(int[] arr, int[][] queries) {
 		for (int row = 0; row < queries.length; row++) {
-			int lt = queries[row][0];
-			int rt = queries[row][1];
+//			int lt = queries[row][0];
+//			int rt = queries[row][1];
+//			
+//			int tmp = arr[lt];
+//
+//			arr[lt] = arr[rt];
+//			arr[rt] = tmp;
 			
-			int tmp = arr[lt];
+			int one = arr[queries[row][0]];
+            int two = arr[queries[row][1]];
 
-			arr[lt] = arr[rt];
-			arr[rt] = tmp;
+            arr[queries[row][0]] = two;
+            arr[queries[row][1]] = one;
 		}
 
 		return arr;
