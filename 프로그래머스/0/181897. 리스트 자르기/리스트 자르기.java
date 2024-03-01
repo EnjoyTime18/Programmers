32bb6bdb75f44aca8d1ac99137bcfbddcaf72169
Seong Min Cho <122413012+EnjoyTime18@users.noreply.github.com>
2024-03-01 20:22:40 +0900

    [level 0] Title: 리스트 자르기, Time: 0.06 ms, Memory: 77 MB -BaekjoonHub

--- 프로그래머스/0/181897. 리스트 자르기/리스트 자르기.java
+++ 프로그래머스/0/181897. 리스트 자르기/리스트 자르기.java
@@ -2,25 +2,53 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 class Solution {
-    public int[] solution(int n, int[] slicer, int[] num_list) {		
-		if (n == 4) {
-			ArrayList<Integer> arraylist = new ArrayList<Integer>();			
-			int[] list = Arrays.copyOfRange(num_list, slicer[0], slicer[1]+1);			
+    public int[] solution(int n, int[] slicer, int[] num_list) {
+//		if (n == 4) {
+//			ArrayList<Integer> arraylist = new ArrayList<Integer>();			
+//			int[] list = Arrays.copyOfRange(num_list, slicer[0], slicer[1]+1);			
+//			for (int i = 0; i < list.length; i += slicer[2]) {
+//				arraylist.add(list[i]);
+//			}
+//			
+//			int[] answer = new int[arraylist.size()];			
+//			for(int i=0; i<arraylist.size(); i++) {
+//				answer[i] = arraylist.get(i);
+//			}			
+//			return answer;			
+//		} else if (n == 3) {
+//			return Arrays.copyOfRange(num_list, slicer[0], slicer[1]+1);
+//		} else if (n == 2) {
+//			return Arrays.copyOfRange(num_list, slicer[0], num_list.length);
+//		} else {
+//			return Arrays.copyOfRange(num_list, 0, slicer[1]+1);
+//		}
+
+		int[] answer = null;
+
+		switch (n) {
+		case 1:
+			answer = Arrays.copyOfRange(num_list, 0, slicer[1] + 1);
+			break;
+		case 2:
+			answer = Arrays.copyOfRange(num_list, slicer[0], num_list.length);
+			break;
+		case 3:
+			answer = Arrays.copyOfRange(num_list, slicer[0], slicer[1] + 1);
+			break;
+		case 4:
+			ArrayList<Integer> arraylist = new ArrayList<Integer>();
+			int[] list = Arrays.copyOfRange(num_list, slicer[0], slicer[1] + 1);
 			for (int i = 0; i < list.length; i += slicer[2]) {
 				arraylist.add(list[i]);
 			}
 			
-			int[] answer = new int[arraylist.size()];			
-			for(int i=0; i<arraylist.size(); i++) {
+			answer = new int[arraylist.size()];
+			for (int i = 0; i < arraylist.size(); i++) {
 				answer[i] = arraylist.get(i);
-			}			
-			return answer;			
-		} else if (n == 3) {
-			return Arrays.copyOfRange(num_list, slicer[0], slicer[1]+1);
-		} else if (n == 2) {
-			return Arrays.copyOfRange(num_list, slicer[0], num_list.length);
-		} else {
-			return Arrays.copyOfRange(num_list, 0, slicer[1]+1);
-		}		
+			}
+			break;
+		}
+
+		return answer;
 	}
 }
